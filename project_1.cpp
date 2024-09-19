02338f89f287762af620e96d7c4f9e20e0f637cb
Simon <SimonVutov1@gmail.com>
2024-09-19 15:43:40 -0400

    p1
    p1

--- project_1.cpp
+++ project_1.cpp
@@ -2,10 +2,10 @@
 #include <cmath>
 
 int main() {
-  float a = -1, b = -1, c = -1, d = -1, e = -1, f = -1, g = -1;
-  float ag = -1, bg = -1, cg = -1, dg = -1, eg = -1, fg = -1, gg = -1;
+  double a = -1, b = -1, c = -1, d = -1, e = -1, f = -1, g = -1;
+  double ag = -1, bg = -1, cg = -1, dg = -1, eg = -1, fg = -1, gg = -1;
 
-  while (a <= 0) {
+  while (a <= 0 || a != std::round(a)) {
     std::cout << "Enter your max grade on the final examination: ";
     std::cin >> a;
   }
@@ -14,7 +14,7 @@ int main() {
     std::cin >> ag;
   }
 
-  while (b <= 0) {
+  while (b <= 0 || b != std::round(b)) {
     std::cout << "Enter your max grade on the midterm examination: ";
     std::cin >> b;
   }
@@ -23,7 +23,7 @@ int main() {
     std::cin >> bg;
   }
 
-  while (c <= 0) {
+  while (c <= 0 || c != std::round(c)) {
     std::cout << "Enter your max grade on project 1: ";
     std::cin >> c;
   }
@@ -32,7 +32,7 @@ int main() {
     std::cin >> cg;
   }
 
-  while (d <= 0) {
+  while (d <= 0 || d != std::round(d)) {
     std::cout << "Enter your max grade on project 2: ";
     std::cin >> d;
   }
@@ -41,7 +41,7 @@ int main() {
     std::cin >> dg;
   }
 
-  while (e <= 0) {
+  while (e <= 0 || e != std::round(e)) {
     std::cout << "Enter your max grade on project 3: ";
     std::cin >> e;
   }
@@ -50,7 +50,7 @@ int main() {
     std::cin >> eg;
   }
 
-  while (f <= 0) {
+  while (f <= 0 || f != std::round(f)) {
     std::cout << "Enter your max grade on project 4: ";
     std::cin >> f;
   }
@@ -59,7 +59,7 @@ int main() {
     std::cin >> fg;
   }
 
-  while (g <= 0) {
+  while (g <= 0 || g != std::round(g)) {
     std::cout << "Enter your max grade on project 5: ";
     std::cin >> g;
   }
@@ -68,13 +68,13 @@ int main() {
     std::cin >> gg;
   }
 
-  float F100 = (ag / a) * 100;
-  float M100 = (bg / b) * 100;
-  float P1100 = (cg / c) * 100;
-  float P2100 = (dg / d) * 100;
-  float P3100 = (eg / e) * 100;
-  float P4100 = (fg / f) * 100;
-  float P5100 = (gg / g) * 100;
+  double F100 = (ag / a) * 100;
+  double M100 = (bg / b) * 100;
+  double P1100 = (cg / c) * 100;
+  double P2100 = (dg / d) * 100;
+  double P3100 = (eg / e) * 100;
+  double P4100 = (fg / f) * 100;
+  double P5100 = (gg / g) * 100;
   if (M100 < F100) M100 = F100;
   if (P1100 < F100) P1100 = F100;
   if (P2100 < F100) P2100 = F100;
@@ -82,12 +82,15 @@ int main() {
   if (P4100 < F100) P4100 = F100;
   if (P5100 < F100) P5100 = F100;
 
-  float E100 = (F100 * 0.75 + M100 * 0.25);
-  float P100 = (P1100 + P2100 + P3100 + P4100 + P5100) / 5;
+  double E100 = (F100 * 0.75 + M100 * 0.25);
+  double P100 = (P1100 + P2100 + P3100 + P4100 + P5100) / 5;
 
-  float final_grade = E100 * (2.0/3.0) + P100 * (1.0/3.0);
-  if (F100 < 60) final_grade = F100;
-  else if (F100 > 40 && F100 < 60) final_grade = P100 * (1.0 / 3.0) * ((E100 - 40.0) / 20.0) + E100 * (1 - ((E100 - 40.0) / 20.0));
+  double final_grade;
+
+  if (F100 >= 60) final_grade = E100 * (2.0/3.0) + P100 * (1.0/3.0);
+  else if (F100 <= 40) final_grade = F100;
+  //else if (F100 > 40 && F100 < 60) final_grade = P100 * (1.0 / 3.0) * ((E100 - 40.0) / 20.0) + E100 * (1 - ((E100 - 40.0) / 20.0));
+  else if (F100 > 40 && F100 < 60) final_grade = (-1.0/60.0)*E100*E100 + (5.0/3.0)*E100 + (1.0/60.0)*P100*E100 - (2.0/3.0)*P100;
 
   int final_grade_int = round(final_grade + 1e-12);
   std::cout << "Your final grade is: " << final_grade_int << std::endl;
